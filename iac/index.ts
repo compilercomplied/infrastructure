3bf06c8e2f4017205bd29039d8f9cd113674ad96
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-06-14 14:33:43 +0200

    disable tailscale

--- iac/index.ts
+++ iac/index.ts
@@ -1,7 +1,6 @@
 import * as k8s from "@pulumi/kubernetes";
 import { configureDocker } from "./modules/docker";
 import { configureMonitoring } from "./monitoring";
-import { configureTailscale } from "./modules/tailscale";
 import { configureAgents } from "./modules/agents";
 import { configureMaintenance } from "./modules/maintenance";
 import { configureSelfhosted } from "./selfhosted";
@@ -11,7 +10,8 @@ const { namespace } = configureAgents();
 
 configureDocker(namespace);
 configureMonitoring();
-configureTailscale();
+// Tailscale resources removed since it is not being used at the moment.
+// configureTailscale();
 configureCertManager();
 configureMaintenance();
 configureSelfhosted();
